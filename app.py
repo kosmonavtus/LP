3a6d735e4258c745d514fba7c6e846fbb1e642bc
black <no_email@email.com>
2022-11-20 16:11:42 +0400

    initial commit

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-#
+from telegram.ext import Updater, CommandHandler
+
+def greet_user(update, context):
+    update.message.reply_text('ТЫ ПИДОР')
+
+
+def main():
+    mybot = Updater('5621425346:AAHKZKfz5sYdQyLNF8UZUvhgmIGnK_ZfMF8')
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+
+    mybot.start_polling()
+    mybot.idle()
+
+
+main()
