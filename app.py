c826f276998392d96f0fd1bda356d9186c6c8f28
black <no_email@email.com>
2022-11-21 11:50:27 +0400

    add talsk to me

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
 token = str(input('insert bot toket hear dev was lazed'))
-from telegram.ext import Updater, CommandHandler
 
 def greet_user(update, context):
     update.message.reply_text('играет музыка из бара "голубая устрица" ')
+
+def talk_to_me(update, context):
+    user_text =  update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
 
 
 def main():
@@ -10,7 +19,9 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
-
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    
+    logging.info("Бот стартовал")
     mybot.start_polling()
     mybot.idle()
 
