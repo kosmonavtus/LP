366fd9da557511ff44b201585e6747816e69e010
black <no_email@email.com>
2022-11-21 10:54:14 +0400

    add list oprtations

---
+++ list.py
@@ -0,0 +1,8 @@
+example_list = [3,5,7,9,10.5]
+print(example_list)
+example_list.append('python')
+print(len(example_list))
+print(example_list[0], example_list[-1])
+print(example_list[1:4])
+example_list.remove('python')
+print(example_list)
