74dcf9e9aad1f07659a1bd8dfbd21a8aa8ed10b6
black <no_email@email.com>
2022-12-03 15:02:23 +0400

    fix multi line

--- lesson3/context_manager.py
+++ lesson3/context_manager.py
@@ -7,8 +7,8 @@ def read_and_write_some_files(inputfile: str, outputfile: str)-> None:
             letters_len += (len(string))
             word_len += (len(string.split()))
             exclamation_marcks += string.replace('.', '!')
-        print(f'Всего букв в файле {file.name}: {letters_len}')
-        print(f'Всего слов в файле {file.name}: {word_len}')
+        print(f'''Всего букв в файле {file.name}: {letters_len} \n 
+                  Всего слов в файле {file.name}: {word_len}''')
 
     with open('lesson3/referat2.txt', 'w') as write_file:
         write_file.write(exclamation_marcks)
