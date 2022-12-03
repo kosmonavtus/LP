1dd31a5828691a5029903cb6acf49b4864f3f951
black <no_email@email.com>
2022-12-03 14:55:27 +0400

    add func

--- lesson3/context_manager.py
+++ lesson3/context_manager.py
@@ -1,19 +1,22 @@
-with open('lesson3/file.txt','r') as file:
-    letters_len = 0
-    word_len = 0
-    exclamation_marcks = ' '
-    for string in file:
-        letters_len += (len(string))
-        word_len += (len(string.split()))
-        exclamation_marcks += string.replace('.', '!')
-    print(f'Всего букв в файле {file.name}: {letters_len}')
-    print(f'Всего слов в файле {file.name}: {word_len}')
+def read_and_write_some_files(inputfile: str, outputfile: str)-> None:
+    with open('lesson3/file.txt','r') as file:
+        letters_len = 0
+        word_len = 0
+        exclamation_marcks = ' '
+        for string in file:
+            letters_len += (len(string))
+            word_len += (len(string.split()))
+            exclamation_marcks += string.replace('.', '!')
+        print(f'Всего букв в файле {file.name}: {letters_len}')
+        print(f'Всего слов в файле {file.name}: {word_len}')
 
-with open('lesson3/referat2.txt', 'w') as write_file:
-    write_file.write(exclamation_marcks)
-    
+    with open('lesson3/referat2.txt', 'w') as write_file:
+        write_file.write(exclamation_marcks)
 
-
+if __name__ == '__main__':
+    inputfile = 'lesson3/file.txt'
+    outputfile = 'lesson3/referat2.txt'
+    read_and_write_some_files(inputfile,outputfile)
 
 
 
