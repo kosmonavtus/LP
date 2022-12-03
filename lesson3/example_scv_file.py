996850f678b5e145b707901891c697af627d41d5
black <no_email@email.com>
2022-12-03 17:28:20 +0400

    add

--- lesson3/example_scv_file.py
+++ lesson3/example_scv_file.py
@@ -13,3 +13,13 @@ with open('lesson3/export.csv', 'w', encoding='utf-8', newline='') as f:
     for user in user_list:
         writer.writerow(user)
         
+lit_of_dict =  [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'}, 
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+]
+with open('lesson3/lit_of_dict.csv', 'w') as lit_of_dict_file:
+    fields2 = ['name', 'age', 'job']
+    csvwriter = csv.DictWriter(lit_of_dict_file, fields2, delimiter=';')
+    for object in lit_of_dict:
+        csvwriter.writerow(object)
